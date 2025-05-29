8d5333347eb5cbbe1d3a3929ecd8b9fa7cfd6b43
Zlata Tikhomirova <72922674+zlatatikhomirova@users.noreply.github.com>
2025-05-29 14:46:54 +0700

    Update join_data.py

--- filter-data/utils/join_data.py
+++ filter-data/utils/join_data.py
@@ -24,8 +24,8 @@ def full_paths(path):
 places_paths =  full_paths(os.path.join(DATA_PATH, 'data-places'))
 imgs_paths = full_paths(os.path.join(DATA_PATH, 'data-images'))
 
-df_places = merge_csvs(places_paths)
-df_imgs = merge_csvs(imgs_paths)
+df_places = merge_csvs(places_paths).reset_index()
+df_imgs = merge_csvs(imgs_paths).reset_index()
 
 df_places.to_csv(os.path.join(DATA_PATH, 'data-merged', 'places_merged.csv'))
 df_imgs.to_csv(os.path.join(DATA_PATH, 'data-merged', 'imgs_merged.csv'))
